86af07292f4396adb306e5b4f8c128a83b98b63c
bregell <johan_bregell@hotmail.com>
2013-02-16 16:02:17 +0100

    Test client Works with the server, Input is i.e. (ip/host, port, DATA) where data is a colon separated list of units i.e. "testID1:testID2". The client will then send mockupdata to the server for each unit each 10sec.

---
+++ Server/src/test_client.erl
@@ -0,0 +1,51 @@
+%% @author Johan
+%% @doc @todo Add description to test_client.
+
+
+-module(test_client).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start/3,worker/2]).
+
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+start(Address, Port, Input) ->
+	case gen_tcp:connect(Address, Port, [list, {active, false}, {packet, 0}]) of
+		{ok, Socket} ->
+			Pid = spawn_link(?MODULE, worker, [Socket, string:tokens(Input, ":")]),
+			Pid ! start,
+			loop(Pid);
+		{error, Reason} ->
+			io:fwrite("Error: "),
+			io:fwrite(Reason)
+	end.
+
+%% @todo Implement connection with database and command builder
+worker(Socket, Units) ->
+	receive
+		start ->
+			Data = fun() -> [random:uniform(500),random:uniform(500),random:uniform(500),random:uniform(500)] end,
+			Convert = fun(A) -> lists:map(fun(B) -> integer_to_list(B) end, A) end,
+			Packet = fun(A) -> A++":"++string:join(Convert(Data()), ";")++":1;1;1;1" end,
+			Send = fun(A) -> gen_tcp:send(Socket, Packet(A)) end,
+			case lists:map(Send(), Units) of
+				ok ->
+					io:fwrite("Data sent \n");
+				{error, Reason} ->
+					io:fwrite("Error: "),
+					io:fwrite(Reason),
+					io:fwrite("\n")
+			end
+	end,
+	worker(Socket, Units).
+
+loop(Pid) ->
+	Time = timer:seconds(10),
+	timer:send_after(Time, Pid, start),
+	loop(Pid).
