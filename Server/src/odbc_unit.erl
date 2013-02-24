68b32a078ee3bb3c5445c7d250eb20680fe03225
bregell <johan_bregell@hotmail.com>
2013-02-25 00:27:05 +0100

    Throws errors correctly and outputs good data values on success.

--- Server/src/odbc_unit.erl
+++ Server/src/odbc_unit.erl
@@ -7,7 +7,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([input/1]).
+-export([input/1,bulk_input/1]).
 
 
 
@@ -16,12 +16,39 @@
 %% ====================================================================
 
 %% @doc 
-%% Takes a list of SQL Strings and sends them to the database.
+%% Takes a list of SQL Strings and sends them to the database one by one.
 %% @end
 %% @spec (Sql) -> (ok() | {error, Reason})
 %% Sql = [string()]
-%% Reason = string()
 input(Sql) ->
 	{ok, Conn} = odbc:connect("DSN=DB", []),
-	lists:foreach(fun(A) -> odbc:sql_query(Conn, A) end, Sql),
+	try (lists:foreach(fun(A) -> throw(odbc:sql_query(Conn, A)) end, Sql)) of
+		{updated, NRows} ->
+			{ok, {updated, NRows}};
+		{selected, ColNames, Rows} ->
+			{ok, {selected, ColNames, Rows}}
+	catch
+		{error, Reason} ->
+			throw({error, Reason})
+	end,
+	odbc:disconnect(Conn).
+
+%% @doc 
+%% Takes a list of SQL Strings and sends them to the database as a bulk operation.
+%% Might be harder to find errors, but faster.
+%% @end
+%% @spec (Sql) -> ({ok,{updated, NRows}} | {ok,{selected, ColNames, Rows}} | {error, Reason})
+%% Sql = [string()]
+bulk_input(Sql) ->
+	{ok, Conn} = odbc:connect("DSN=DB", []),
+	Bulk = string:join(Sql, ";"),
+	try (odbc:sql_query(Conn, Bulk)) of
+		{updated, NRows} ->
+			{ok, {updated, NRows}};
+		{selected, ColNames, Rows} ->
+			{ok, {selected, ColNames, Rows}}
+	catch
+		{error, Reason} ->
+			throw({error, Reason})
+	end,
 	odbc:disconnect(Conn).
