a818b890e5ae078489cb1d7ee6274a26ee46288d
bregell <johan_bregell@hotmail.com>
2013-02-26 21:06:48 +0100

    Big start should do most of the desired things fine. Gets a list from database with history and then transforms it into a list of booleans for each port and then mends them to the controller. This will later go trough the command builder.

--- Server/src/analyzer.erl
+++ Server/src/analyzer.erl
@@ -8,7 +8,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([start/0, worker/1]).
+-export([start/0, worker/2, split/3, analyzer/2]).
 
 
 
@@ -20,10 +20,43 @@ start() ->
 
 mailbox() ->
 	receive
-		{read,SID} ->
-			spawn_link(?MODULE, worker, [SID])
+		{read, SID, Length} ->
+			spawn_link(?MODULE, worker, [SID, Length])
 	end,
 	mailbox().
 
-worker(SID) ->
-	io:fwrite(sql_builder:input([SID])).
+worker(SID, Length) ->
+	try sql_builder:input({SID, Length}) of
+		{ok,[Answer]} ->
+			Status = analyzer(Answer, Length),
+			controller ! {send,{SID, Status}}
+	catch 
+		{error,_} ->
+			io:fwrite("Error when sending to SQL_builder\n");
+		_ -> 
+			io:fwrite("Strange data from SQL Module\n")
+	end.
+
+
+analyzer(Answer, Length) ->
+	{selected,_,Data} = Answer,
+	[Sorted] = split(lists:keysort(2,Data), [], Length),
+	Function = fun(B) -> B >= 5 end,
+	[lists:member(true, [Function(Y) || {_,_,_,Y} <- N]) || N <- Sorted].
+
+split(List, [], Length) when length(List) < Length div 4 ->
+	[List];
+split(List, Ans, Length) when length(List) < Length div 4 ->
+	case List of
+		[] ->
+			[Ans];
+		_ ->
+			[List|Ans]
+	end;
+split(List, [], Length) ->
+	{X,Y} = lists:split(Length div 4, List),
+	split(Y, [X], Length);
+split(List, Ans, Length) when length(List) >= Length div 4 ->
+	{X,Y} = lists:split(Length div 4, List),
+	split(Y, [X|Ans], Length).
+
