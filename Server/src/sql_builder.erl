bc72191c6efb2b5930ae07b7204005a8418601f6
bregell <johan_bregell@hotmail.com>
2013-03-20 11:23:57 +0100

    Converted to Postgres syntax

--- Server/src/sql_builder.erl
+++ Server/src/sql_builder.erl
@@ -36,7 +36,8 @@ input(Input) ->
 					throw({error, Reason})	
 			end;
 		{SID, Length} ->
-			SQL = ("SELECT (powerStrip_id, socket_id, timeStamp, activePower) FROM powerStrip_consumption WHERE powerStrip_id='"++SID++"' ORDER BY timeStamp DESC LIMIT 0,"++integer_to_list(Length)),
+			%%SELECT id, socket_id, "powerStrip_id", "activePower", "timeStamp" FROM "powerStrip_consumption";
+			SQL = ("SELECT (\"powerStrip_id\", socket_id, \"timeStamp\", \"activePower\") FROM \"powerStrip_consumption\" WHERE \"powerStrip_id\"='"++SID++"' ORDER BY \"timeStamp\" DESC LIMIT 0,"++integer_to_list(Length)),
 			try (odbc_unit:input([SQL])) of
 				{ok, Answer} ->
 					{ok, Answer}
@@ -90,7 +91,7 @@ new_data(SID, Data) ->
 	
 	%% INSERT INTO consumption (serialId, id, activepower, timestamp) VALUES [(SID, Data[n], Status[n], NOW())]
 	%% Adds the SQL syntax
-	["INSERT INTO powerStrip_consumption (powerStrip_id, activePower, socket_id, timeStamp) VALUES "++Values].
+	["INSERT INTO \"owerStrip_consumption\" (\"powerStrip_id\", \"activePower\", socket_id, \"timeStamp\") VALUES "++Values].
 
 %% @doc
 %% This function is called when the input is new statuses for the unit. 
@@ -103,5 +104,6 @@ new_status(SID, Status) ->
 	Id = ["1", "2", "3", "4"],
 	
 	%% Makes a list of UPDATE statements for the given SID
-	Make = fun(B) -> lists:map(fun({C,D}) -> "UPDATE powerStrip_socket SET status="++D++" WHERE powerStrip_id='"++SID++"' AND socket='"++C++"'" end, lists:zip(Id, B)) end,
+    %% UPDATE "powerStrip_socket" SET id=?, socket=?, "powerStrip_id"=?, status=?, name=? WHERE <condition>;
+	Make = fun(B) -> lists:map(fun({C,D}) -> "UPDATE \"powerStrip_socket\" SET status="++D++" WHERE \"powerStrip_id\"='"++SID++"' AND socket='"++C++"'" end, lists:zip(Id, B)) end,
 	Make(Status).
