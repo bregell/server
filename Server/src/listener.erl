3b9f1bc7acfffd325f7f19f35dfe1ac2c85719ec
Johan Bregell <johan@bregell.se>
2013-04-11 15:14:35 +0200

    Spawns new listener directly after recieving new package

--- Server/src/listener.erl
+++ Server/src/listener.erl
@@ -72,8 +72,8 @@ receiver(Socket) ->
 	io:fwrite("Waiting for package\n"),
 	%% @todo Add some good timeout value maybe.
 	case gen_tcp:recv(Socket, 0) of
-		spawn_link(?MODULE, receiver, [Socket]),
 		{ok, Package} ->
+			spawn_link(?MODULE, receiver, [Socket]),
 			io:fwrite("Recieve OK\n"),
 			%% Parse string into list 
 			Output = string:tokens(Package, ":"),
@@ -109,6 +109,7 @@ receiver(Socket) ->
 					io:fwrite("\n")
 			end;
 		{error, Reason} ->
+			spawn_link(?MODULE, receiver, [Socket]),
 			io:fwrite("Could not recieve!\n"),
 			io:fwrite(Reason),
 			io:fwrite("\n")
