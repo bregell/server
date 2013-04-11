9825a37206ffcaac82373c087cd7e9957bbe7e52
bregell <johan_bregell@hotmail.com>
2013-04-11 16:22:13 +0200

    Now spawns sql_builder in new thread

--- Server/src/listener.erl
+++ Server/src/listener.erl
@@ -82,17 +82,7 @@ receiver(Socket) ->
 					io:fwrite("\n"),
 					controller ! {new,{PowerStrip_SerialId,Socket}},
 					%% @issue Maybe cange this to some kind of message passing solution.
-					try (sql_builder:input([PowerStrip_SerialId,string:tokens(Data, ";"),string:tokens(Status, ";"),string:tokens(Date, ";"),string:tokens(Time, ";")])) of
-						{ok, _} ->
-							io:fwrite("Data saved without problems!\n")
-					catch
-						{error, Reason} ->
-							io:fwrite("Error when saved data!\n"),
-							io:fwrite(Reason),
-							io:fwrite("\n");
-						_ ->
-							io:fwrite("Strange things is happening!\n")
-					end,
+					spawn(sql_builder, input, [[PowerStrip_SerialId,string:tokens(Data, ";"),string:tokens(Status, ";"),string:tokens(Date, ";"),string:tokens(Time, ";")]]),
 					analyzer ! {read, PowerStrip_SerialId};
 				[PowerStrip_SerialId, Status] ->
 					odbc_unit:input(sql_builder:new_status(PowerStrip_SerialId, string:tokens(Status, ";"))),
